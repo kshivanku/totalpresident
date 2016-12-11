1a16d394d7d3c9c4dd117ebb633807ec2887b0d8
kshivanku <kshivanku@gmail.com>
2016-12-11 05:21:57 -0500

    adding config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'wLMnqcmihA4NcoIcWqRjMhq0A',
+  consumer_secret:      'xa77BRFXSjJZB4wckUiuyXYTge3V4yfkjUQDYauWN42y3MGaMl',
+  access_token:         '805519834012327936-VokLSDDJmA1YPzrJQ1g3iRpJfowylLm',
+  access_token_secret:  'gWkWVYyUwSTlVVCTWwlH9B6O6YnHFQvduYIBg020AWA5q',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
