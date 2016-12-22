829465fe66626d2428b573986c1557164cc5cc66
kshivanku <kshivanku@gmail.com>
2016-12-21 21:30:16 -0500

    changing access tokens

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   consumer_key:         'ggP1BoyI0p71NvYNJNMKJLbTJ',
   consumer_secret:      'Fp1NUcLgxu0vkxlPvyXmCeKXElVutHeTL4ljRcAqR5JKGkteNF',
-  access_token:         '810538589251653632-7s6q3eLOCoBaOrXE7tnF9IOsVHJqSun',
-  access_token_secret:  'YvK6YbRaX0RNtehWasYuhyBPXf1MDOxLNQZfrjs0kkSH8',
+  access_token:         '810538589251653632-DT7L539IBIVMZtxWRLLhPJKh75rqyTw',
+  access_token_secret:  '6BzGa0C06fMO3SkQaClGUlvfhYaXpVawsVGqAkI4j1CJH',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
