bab753e80797754919eb2206ee815b92e444455c
kshivanku <kshivanku@gmail.com>
2016-12-21 21:35:36 -0500

    changing access tokens

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  consumer_key:         'ggP1BoyI0p71NvYNJNMKJLbTJ',
-  consumer_secret:      'Fp1NUcLgxu0vkxlPvyXmCeKXElVutHeTL4ljRcAqR5JKGkteNF',
-  access_token:         '810538589251653632-DT7L539IBIVMZtxWRLLhPJKh75rqyTw',
-  access_token_secret:  '6BzGa0C06fMO3SkQaClGUlvfhYaXpVawsVGqAkI4j1CJH',
+  consumer_key:         'jRpfWq0fwSgPnCSHbwY9m5jlS',
+  consumer_secret:      'Tlq58KzWe0kEcRCbPlZf2G7teskMeeRjawitykh0k9hcBsoRhm',
+  access_token:         '810538589251653632-mGxwH9wvm6I3zKV6MfmbMxyNjvO1ktU',
+  access_token_secret:  'VGpEmsnC3Nec0cqAHH2nc9h9f6FsbQtXGmeRQ29WUCWNl',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
